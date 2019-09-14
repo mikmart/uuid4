0291f3db122c91325fd63b9fdf6e01594dd206c2
Mikko Marttila <mikmart@mikkomarttila.com>
2019-09-14 12:01:06 +0300

    Run document()

---
+++ R/uuid4-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @useDynLib uuid4, .registration = TRUE
+## usethis namespace: end
+NULL
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
