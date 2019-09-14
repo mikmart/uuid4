668480aa3d11f2c13a95be5cfcf208fec26e5375
Mikko Marttila <mikmart@mikkomarttila.com>
2019-09-14 19:57:28 +0300

    Elaborate documentation

--- R/uuid.R
+++ R/uuid.R
@@ -1,8 +1,20 @@
 #' Generate a character vector of UUID4s
+#'
+#' A UUID4 is a random 128-bit integer with 6 bits containing fixed version
+#' information. They are surfaced to R as a character vector containing the
+#' hexadecimal representation. UUIDs are generated with a PCG random number
+#' generator, separate from R's built-in RNGs, so `set.seed()` has no effect on
+#' `uuid()`.
+#'
 #' @param n number of UUIDs to generate
 #' @return a character vector of length `n`
-#' @seealso [RFC 4122](https://tools.ietf.org/html/rfc4122) for the specification
+#' @seealso [RFC 4122](https://tools.ietf.org/html/rfc4122) for the
+#'   specification. [http://www.pcg-random.org]() for the RNG used.
 #' @export
+#' @examples
+#' library(uuid4)
+#'
+#' uuid() # a single UUID4
 uuid <- function(n = 1L) {
   .Call(C_uuid4, as.integer(n))
 }
